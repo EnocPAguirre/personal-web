4acf68db9147525ede4cfc100460ec72920e1e86
EnocPAguirre <enocpinedaa@gmail.com>
2023-09-24 00:18:05 -0600

    experience

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { HeaderComponent } from './header/header.component';
+import { SharedModule } from '../shared/shared.module';
+import { ExperienceComponent } from './experience/experience.component';
 
 
 
 @NgModule({
   declarations: [
-    WelcomeComponent
+    WelcomeComponent,
+    HeaderComponent,
+    ExperienceComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    SharedModule
+  ],
 })
 export class ComponentsModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
-
+const components = [
+  CommonModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatCardModule
+]
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: components,
+  exports: components
+    
 })
 export class SharedModule { }
